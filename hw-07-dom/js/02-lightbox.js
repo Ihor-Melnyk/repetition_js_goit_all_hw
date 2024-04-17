783473d6b60d7c01ea5af9d5c0d12b97d951d246
Ihor Melnyk <igor.melnick85@gmail.com>
2024-04-18 00:03:03 +0300

    added task2 hw07

--- hw-07-dom/js/02-lightbox.js
+++ hw-07-dom/js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const markUp = galleryItems
+  .map(
+    ({ description, original, preview }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join("");
+const ulElem = document.querySelector(".gallery");
+ulElem.insertAdjacentHTML("beforeend", markUp);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
